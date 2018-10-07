243919a97f0dd1c7a9f8f2e43fcb5f0a5045b98f
Andrew <kirilenko.haa@gmail.com>
2018-10-07 21:02:05 +0300

    RT-iOS-002 favorites form

---
+++ Reading Tracker/Reading Tracker/Registration/RegistrationFavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegistrationFavoritesViewController.swift
+//  Reading Tracker
+//
+//  Created by Andrei Kirilenko on 07.10.2018.
+//  Copyright © 2018 Andrei Kirilenko. All rights reserved.
+//
+
+import Foundation
