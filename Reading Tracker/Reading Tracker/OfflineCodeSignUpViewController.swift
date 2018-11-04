5c379d6feddb73d531cd6a9849a7d21751a0b154
Andrew <kirilenko.haa@gmail.com>
2018-11-04 18:40:06 +0300

    RT-iOS: subviews init

--- Reading Tracker/Reading Tracker/OfflineCodeSignUpViewController.swift
+++ Reading Tracker/Reading Tracker/OfflineCodeSignUpViewController.swift
@@ -13,6 +13,7 @@ import Firebase
 
 class OfflineCodeSignUpViewController: UIViewController {
     private var spinner: UIActivityIndicatorView?
+    private var codeTextField: RTTextField?
     private let codeTextFieldDelegate = FinishTextFieldDelegate()
     
     override func viewDidLoad() {
@@ -52,23 +53,59 @@ class OfflineCodeSignUpViewController: UIViewController {
             NSAttributedString.Key.font : UIFont(name: "Avenir-Light", size: 20.0)!]
             as [NSAttributedString.Key : Any]
         
-        let codeTextField = RTTextField()
+        let codeTextField = RTTextField(padding: .zero)
         codeTextField.attributedPlaceholder = NSAttributedString(string: "Код участника", attributes: placeholderTextAttributes)
         codeTextField.backgroundColor = .clear
         codeTextField.autocorrectionType = .no
         codeTextField.keyboardType = .decimalPad
         codeTextField.delegate = codeTextFieldDelegate
         codeTextField.returnKeyType = .done
+        
+        view.addSubview(codeTextField)
+        codeTextField.autoAlignAxis(toSuperviewAxis: .vertical)
+        codeTextField.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 210, left: 16, bottom: 0, right: 0), excludingEdge: .bottom)
+        
+        self.codeTextField = codeTextField
+        
+        let lineView = UIView(frame: .zero)
+        lineView.backgroundColor = UIColor(rgb: 0x2f5870)
+        
+        view.addSubview(lineView)
+        lineView.autoPinEdge(.top, to: .bottom, of: codeTextField, withOffset: 8)
+        lineView.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        lineView.autoPinEdge(toSuperviewEdge: .right, withInset: 16)
+        lineView.autoSetDimension(.height, toSize: 1)
+        
+        let activateButton = UIButton(frame: .zero)
+        
+        let buttonTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.white,
+            NSAttributedString.Key.font : UIFont(name: "Avenir-Medium", size: 20.0)!]
+            as [NSAttributedString.Key : Any]
+        
+        activateButton.setAttributedTitle(NSAttributedString(string: "Активировать код", attributes: buttonTextAttributes), for: .normal)
+        activateButton.backgroundColor = UIColor(rgb: 0x2f5870)
+        activateButton.layer.cornerRadius = 32
+        activateButton.addTarget(self, action: #selector(onActivateButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(activateButton)
+        activateButton.autoPinEdge(toSuperviewEdge: .bottom, withInset: 100)
+        activateButton.autoSetDimensions(to: CGSize(width: 274, height: 64))
+        activateButton.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+    }
+    
+    @objc private func onActivateButtonTapped() {
+        //todo: activation
+        let code = codeTextField?.text ?? ""
+        print("Code: \(code)")
+        navigationController?.popViewController(animated: true)
     }
     
     private class FinishTextFieldDelegate: NSObject, UITextFieldDelegate {
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             textField.resignFirstResponder()
             return true
-        }
-        
-        func textFieldDidEndEditing(_ textField: UITextField) {
-            //TODO: check corectness
         }
     }
 }
