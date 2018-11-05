ea9b6e8996cdc2a03d4dac1eff6787edccd07831
Andrew <kirilenko.haa@gmail.com>
2018-11-05 21:01:09 +0300

    RT-iOS: add books table

--- Reading Tracker/Reading Tracker/Library/MyBooksViewController.swift
+++ Reading Tracker/Reading Tracker/Library/MyBooksViewController.swift
@@ -9,8 +9,25 @@
 import Foundation
 import UIKit
 
-final class MyBooksViewController: UIViewController {
+public struct BookModel {
+    public var title: String
+    public var author: String
+    public var image: UIImage
+    public var lastUpdated: Date
+    
+    init(title: String, author: String, image: UIImage? = nil, lastUpdated: Date = Date.distantPast) {
+        self.title = title
+        self.author = author
+        self.image = (image ?? UIImage(named: "bookPlaceholder")) ?? UIImage()
+        self.lastUpdated = lastUpdated
+    }
+}
+
+final class MyBooksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     private var spinner: UIActivityIndicatorView?
+    private var books: [BookModel] = [BookModel(title: "Биоцентризм. Как жизнь создает вселенную", author: "Роберт Ланца"),
+                                       BookModel(title: "Фату-Хива: возврат к природе", author: "Тур Хейердал")]
+    private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +35,17 @@ final class MyBooksViewController: UIViewController {
         view.backgroundColor = .white
         setupNavigationBar()
         setupSpinner()
+        
+        tableView = UITableView(forAutoLayout: ())
+        view.addSubview(tableView)
+        tableView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: UIApplication.shared.statusBarFrame.height + 4 + 32, left: 0, bottom: 0, right: 0))
+        tableView.register(BookCell.self, forCellReuseIdentifier: "bookCell")
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .white
+        tableView.separatorInset = .zero
+        tableView.separatorColor = UIColor(rgb: 0x2f5870)
     }
     
     private func setupNavigationBar() {
@@ -43,5 +71,101 @@ final class MyBooksViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: BookCell = self.tableView.dequeueReusableCell(withIdentifier: "bookCell") as! BookCell
+        cell.configure(model: books[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    private class BookCell: UITableViewCell {
+        private var model: BookModel = BookModel(title: "", author: "")
+        private var titleLabel: UILabel?
+        private var authorLabel: UILabel?
+        private var coverImageView: UIImageView?
+        
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            backgroundColor = .white
+            selectionStyle = .none
+            setupSubviews()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private func setupSubviews() {
+            let titleLabel = UILabel(forAutoLayout: ())
+            
+            let titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x2f5870),
+                NSAttributedString.Key.font : UIFont(name: "Avenir-Medium", size: 20.0)!]
+                as [NSAttributedString.Key : Any]
+            
+            titleLabel.attributedText = NSAttributedString(string: model.title, attributes: titleTextAttributes)
+            titleLabel.numberOfLines = 0
+            
+            let authorLabel = UILabel(forAutoLayout: ())
+            
+            let authorTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x2f5870),
+                NSAttributedString.Key.font : UIFont(name: "Avenir-Medium", size: 14.0)!]
+                as [NSAttributedString.Key : Any]
+            
+            authorLabel.attributedText = NSAttributedString(string: model.author, attributes: authorTextAttributes)
+            authorLabel.numberOfLines = 0
+            
+            let coverImageView = UIImageView(image: model.image)
+            coverImageView.contentMode = .scaleAspectFill
+            
+            addSubview(titleLabel)
+            
+            titleLabel.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+            titleLabel.autoPinEdge(toSuperviewEdge: .right, withInset: 16 + 70 + 16)
+            titleLabel.autoPinEdge(toSuperviewEdge: .top, withInset: 20)
+            
+            addSubview(authorLabel)
+            
+            authorLabel.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+            authorLabel.autoPinEdge(toSuperviewEdge: .right, withInset: 16 + 70 + 16)
+            authorLabel.autoPinEdge(.top, to: .bottom, of: titleLabel, withOffset: 5)
+            
+            addSubview(coverImageView)
+            
+            coverImageView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 16), excludingEdge: .left)
+            coverImageView.autoSetDimensions(to: CGSize(width: 70, height: 100))
+            
+            self.titleLabel = titleLabel
+            self.authorLabel = authorLabel
+            self.coverImageView = coverImageView
+        }
+        
+        func configure(model: BookModel) {
+            self.model = model
+            
+            let titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x2f5870),
+                NSAttributedString.Key.font : UIFont(name: "Avenir-Medium", size: 20.0)!]
+                as [NSAttributedString.Key : Any]
+            
+            let authorTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x2f5870),
+                NSAttributedString.Key.font : UIFont(name: "Avenir-Medium", size: 14.0)!]
+                as [NSAttributedString.Key : Any]
+            
+            titleLabel?.attributedText = NSAttributedString(string: model.title, attributes: titleTextAttributes)
+            authorLabel?.attributedText = NSAttributedString(string: model.author, attributes: authorTextAttributes)
+            coverImageView?.image = model.image
+        }
     }
 }
