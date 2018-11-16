80e3cd928cb36428d57141ac3db94f837d6b092d
Andrew <kirilenko.haa@gmail.com>
2018-11-16 18:59:58 +0300

    RT-iOS: update

--- Reading Tracker/Reading Tracker/Utils/ImageCompression.swift
+++ Reading Tracker/Reading Tracker/Utils/ImageCompression.swift
@@ -11,21 +11,7 @@ import UIKit
 
 extension UIImage {
     func resizeImage(targetSize: CGSize) -> UIImage {
-        let widthRatio  = targetSize.width  / size.width
-        let heightRatio = targetSize.height / size.height
-        
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        }
-        
-        // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height)
-        
-        // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(targetSize, false, 1.0)
         self.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
