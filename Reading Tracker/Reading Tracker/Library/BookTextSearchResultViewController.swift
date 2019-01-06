703b7ff0a5d8d03b8a83bc6399d2232eaa849df3
Andrew <kirilenko.haa@gmail.com>
2019-01-07 00:18:36 +0300

    RT-iOS: small refactor

---
+++ Reading Tracker/Reading Tracker/Library/BookTextSearchResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BookTextSearchResultViewController.swift
+//  Reading Tracker
+//
+//  Created by Andrei Kirilenko on 07/01/2019.
+//  Copyright © 2019 Andrei Kirilenko. All rights reserved.
+//
+
+import Foundation
